0a553716b59bf7ed993b56934ffd096bdf309f17
blue-putty <65861156+blue-putty@users.noreply.github.com>
2021-11-20 11:15:01 -0500

    test

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Blue Putty</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
